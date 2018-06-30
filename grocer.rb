978c001902cb3d268061657aae5d79ebd028f3f6
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 03:44:32 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,30 +15,37 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     product = coupon[:item]
     if cart[product] && cart[product][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-      cart["#{name} W/COUPON"][:count] += 1
+      if cart["#{product} W/COUPON"]
+        cart["#{product} W/COUPON"][:count] += 1
       else
-      
+        price = coupon[:cost]
+        clearance = cart[product][:clearance]
+        cart["#{product} W/COUPON"] = {
+          count: 1,
+          price: price
+        }
+        cart["#{product} W/COUPON"][:clearance] = clearance
       end
+      cart[name][:count] -= coupon[:num]
     end
   cart
 end
 
-def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
-      else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
-      end
-      cart[name][:count] -= coupon[:num]
-    end
-  end
-  cart
-end
+# def apply_coupons(cart, coupons)
+#   coupons.each do |coupon|
+#     name = coupon[:item]
+#     if cart[name] && cart[name][:count] >= coupon[:num]
+#       if cart["#{name} W/COUPON"]
+#         cart["#{name} W/COUPON"][:count] += 1
+#       else
+#         cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+#         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+#       end
+#       cart[name][:count] -= coupon[:num]
+#     end
+#   end
+#   cart
+# end
 
 def apply_clearance(cart)
   # code here
