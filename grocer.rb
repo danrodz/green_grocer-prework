f6dabeec82c5a30b4f23b009a8b3828bdc9bb57b
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-30 02:53:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,51 +1,47 @@
-
 def consolidate_cart(cart)
-  cart.each_with_object({}) do |item, result|
-    item.each do |type, attributes|
-      if result[type]
-        attributes[:count] += 1
+  # code here
+  consolidated_cart = {}
+  cart.each do |products|
+    products.each do |product, product_hash|
+      if !consolidated_cart.has_key?(product)
+        consolidated_cart[product] = {
+          price: 0.0,
+          clearance: false,
+          count: 1
+        }
       else
-        attributes[:count] = 1
-        result[type] = attributes
+        consolidated_cart[product][:count] += 1
+      end
+      product_hash.each do |key, value|
+        consolidated_cart[product][key] = value
       end
     end
   end
+  consolidated_cart
 end
 
-def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
-      else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+def apply_coupons(cart, *coupons)
+  products_with_coupons = {}
+  coupons.each do |coupon_hash|
+    cart.each do |product, product_hash|
+      if product == product_hash[:item] && product_hash[:count] >= coupon_hash[:num]
+        product_hash[:count] = product_hash[:count] - coupon_hash[:num]
+        new_product = coupon_hash[:item] + " w/coupon"
+        products_with_coupons[new_product] = {
+          price: coupon_hash[:cost],
+          count: 1,
+          clearance: product_hash[:clearance]
+        }
       end
-      cart[name][:count] -= coupon[:num]
     end
   end
-  cart
+  cart.merge(products_with_coupons)
 end
 
 def apply_clearance(cart)
-  cart.each do |name, properties|
-    if properties[:clearance]
-      updated_price = properties[:price] * 0.80
-      properties[:price] = updated_price.round(2)
-    end
-  end
-  cart
+  # code here
 end
 
 def checkout(cart, coupons)
-  consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart, coupons)
-  final_cart = apply_clearance(couponed_cart)
-  total = 0
-  final_cart.each do |name, properties|
-    total += properties[:price] * properties[:count]
-  end
-  total = total * 0.9 if total > 100
-  total
+  # code here
 end
